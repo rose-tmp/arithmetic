41b720837e62cb564fca95fe9e16e21556bb7bb7
ZwZ <2533725595@qq.com>
2020-04-01 23:12:54 +0800

    2020/4/1将arithmetic交给git管理

--- src/zuoPrimary/shu/CompleteBinaryTreeNodeSum.java
+++ src/zuoPrimary/shu/CompleteBinaryTreeNodeSum.java
@@ -1,5 +1,7 @@
 package zuoPrimary.shu;
 
+import zuoPrimary.Swap;
+
 /**
  * @author - ZwZ
  * @date - 2020/3/25 - 22:46
@@ -77,5 +79,34 @@ public class CompleteBinaryTreeNodeSum {
         root.right.right.left = new TreeNode(7);
         root.right.right.left.right = new TreeNode(4);
         System.out.println(sum.nodeSum(root));
+    }
+
+    /**
+     * @author - ZwZ
+     * @date - 2020/3/22 - 15:03
+     * @Description:选择排序
+     * (由于时间复杂度高，这种排序算法几乎没有人使用在工程中)
+     * 0-----n-1选出最小值放在0位置
+     * 1-----n-1选出最小值放在1位置
+     * 2-----n-1选出最小值放在2位置
+     * ....
+     * 时间复杂度和数据本身状况无关，恒为：O(n^2)
+     *
+     * 无论如何都做不到稳定 举例：5 5 5 5 4 0 2
+     */
+    public static class SelectSort {
+        public void selectSort(int[] arr){
+            if(arr == null || arr.length < 2)
+                return;
+            for (int start = 0; start < arr.length - 1; start++) {
+                int min = start;
+                //找最小值的下标
+                for (int i = start + 1; i < arr.length; i++) {
+                    min = arr[i] < arr[min] ? i : min;
+                }
+                //交换
+                Swap.swap(arr, start, min);
+            }
+        }
     }
 }
