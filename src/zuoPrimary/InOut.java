1034d8748d68f0644be00e2629e0e4c9dce5bd68
ZwZ <2533725595@qq.com>
2020-04-02 13:04:05 +0800

    InOut控制台输入输出

--- src/zuoPrimary/InOut.java
+++ src/zuoPrimary/InOut.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class InOut {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //题目中要求几行输入，就定义几个String变量 如果是整型 使用Inteager.parseInt(String str)转
         String first = "";
         String second = "";
         for (int i = 0; i < 2; i++) {
