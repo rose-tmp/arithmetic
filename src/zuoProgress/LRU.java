ce232fe1345cf91a034b6548db9997639152589c
ZwZ <2533725595@qq.com>
2020-04-20 10:36:07 +0800

    LRU

---
+++ src/zuoProgress/LRU.java
@@ -0,0 +1,15 @@
+package zuoProgress;
+
+/**
+ * @author - zwz
+ * @date - 20-4-14-下午8:02
+ * @Description:设计可以变更的缓存结构(LRU)
+ * 该结构在构造时确定大小，假设大小为K,并有两个功能，set(key,value) get(key)
+ * 要求：
+ *  1.set和get的时间复杂度为O(1)
+ *  2.某个key的set或get操作一旦发生，认为这个keyde的记录成了最近经常使用的
+ *  3.当缓存的大小超过K时，移除最不经常使用的记录，即set或get最久远的
+ */
+public class LRU {
+    private
+}
