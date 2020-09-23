4d9edca0ff379f749d09f5b37750aa47ba97fd6d
ZwZ <2533725595@qq.com>
2020-09-23 21:40:59 +0800

    最长回文子串
    给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
    
    示例 1：
    
    输入: "babad"
    输出: "bab"
    注意: "aba" 也是一个有效答案。
    示例 2：
    
    输入: "cbbd"
    输出: "bb"
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/longest-palindromic-substring

--- src/leetcode/pool/TreeNode.java
+++ src/leetcode/pool/TreeNode.java
@@ -9,7 +9,7 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    TreeNode(int x) {
-        val = x;
+    public TreeNode(int x) {
+        this.val = x;
     }
 }
