c896ca4ebbc8e23aa071a06403d55e74770a80d9
ZwZ <2533725595@qq.com>
2020-09-24 20:23:10 +0800

    给定一个有相同值的二叉搜索树（BST），找出 BST 中的所有众数（出现频率最高的元素）。

--- src/leetcode/FindMode.java
+++ src/leetcode/FindMode.java
@@ -29,53 +29,40 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/find-mode-in-binary-search-tree
  */
 public class FindMode {
-    List<Integer> mList = new ArrayList<>();
-    int curent = 0;//表示当前节点的值
+    List<Integer> List = new ArrayList<>();
+    int pre = 0;//中序遍历时当前节点的上一个节点
     int count = 0;//表示当前节点的数量
     int maxCount = 0;//最大的重复数量
 
     public int[] findMode(TreeNode root) {
         inOrderTraversal(root);
-        int[] res = new int[mList.size()];
+        int[] res = new int[List.size()];
         //把集合list转化为数组
-        for (int i = 0; i < mList.size(); i++) {
-            res[i] = mList.get(i);
+        for (int i = 0; i < List.size(); i++) {
+            res[i] = List.get(i);
         }
         return res;
     }
-
-    //递归方式
-    public void inOrderTraversal(TreeNode node) {
-        //终止条件判断
-        if (node == null)
+    public void inOrderTraversal(TreeNode root) {
+        if (root == null)
             return;
-        //遍历左子树
-        inOrderTraversal(node.left);
-
-        //下面是对当前节点的一些逻辑操作
-        int nodeValue = node.val;
-        if (nodeValue == curent) {
-            //如果节点值等于curent，count就加1
+        inOrderTraversal(root.left);
+        //对当前节点的一些逻辑操作
+        int rootVal = root.val;
+        if (rootVal == pre) {
             count++;
         } else {
-            //否则，就表示遇到了一个新的值，curent和count都要
-            //重新赋值
-            curent = nodeValue;
+            pre = rootVal;
             count = 1;
         }
         if (count == maxCount) {
-            //如果count == maxCount，就把当前节点加入到集合中
-            mList.add(nodeValue);
+            List.add(rootVal);
         } else if (count > maxCount) {
-            //否则，当前节点的值重复量是最多的，直接把list清空，然后
-            //把当前节点的值加入到集合中
-            mList.clear();
-            mList.add(nodeValue);
+            List.clear();
+            List.add(rootVal);
             maxCount = count;
         }
-
-        //遍历右子树
-        inOrderTraversal(node.right);
+        inOrderTraversal(root.right);
     }
 
     public static void main(String[] args) {
