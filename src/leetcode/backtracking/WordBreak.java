86eba1070807f76de95090d35f3f4a41d5837696
ZwZXXX <wenzhe.zhang@njust.edu.cn>
2020-11-01 20:35:15 +0800

    140. 单词拆分 II

--- src/leetcode/backtracking/WordBreak.java
+++ src/leetcode/backtracking/WordBreak.java
@@ -1,8 +1,6 @@
 package leetcode.backtracking;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author - ZwZ
@@ -48,57 +46,101 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/word-break-ii
  */
 public class WordBreak {
-    public List<String> wordBreak(String s, List<String> wordDict) {
-        List<Integer> lengths = new ArrayList<>();
-        //收集wordDicts中每一个单词的长度并存储在lenghs中(不重复)
-        for (int i = 0; i < wordDict.size(); i++) {
-            int len = wordDict.get(i).length();
-            //如果lengths中不存在len则加入
-            boolean flag = false;
-            for (int j = 0; j < lengths.size(); j++) {
-                if (lengths.get(j) == len) {
-                    flag = true;
-                    continue;
-                }
-            }
-            if (flag == true)
-                continue;
-            else {
-                lengths.add(len);
-            }
+    /**
+     * 完全使用回溯算法解决
+     * 参考 https://leetcode-cn.com/problems/word-break-ii/solution/hui-su-fa-he-ji-yi-hua-sou-suo-by-si-zhai-bu-tai-f/
+     */
+    public List<String> wordBreak1(String s, List<String> wordDict) {
+        List<String> res = new ArrayList<>();
+        Set<String> wordSet = new HashSet<>();
+        for (String word : wordDict) {
+            wordSet.add(word);
         }
-        //将集合中的元素加入数组 方便接下来使用sort()排序
-        int[] lens = new int[lengths.size()];
-        for (int i = 0; i < lengths.size(); i++) {
-            lens[i] = lengths.get(i);
+        if (s.length() == 0) {
+            return res;
         }
-        Arrays.sort(lens);
+        backTrack(res, new StringBuilder(), s, wordSet, 0, 0);
+        return res;
     }
 
     /**
-     * lengths:记录wordDict中所有单词出现过的长度
-     * indexLengths:lengths中的下标
-     * indexPre:指向当前匹配的s中某个连续字符串(单词)的第一个下标
-     * indexRear:指向当前匹配的s中某个连续字符串(单词)的最后一个下标
+     * begin:匹配开始时 s字符串的下标
+     * countBlankSpace:加入的空格的数量，作为删除某个单词时的偏移量
      */
-    public void backTrack(List<String> res, StringBuilder sb, String s, List<String> wordDict,
-                          int[] lens, int indexLens, int indexPre, int indexRear) {
+    public void backTrack(List<String> res, StringBuilder sb, String s,
+                          Set<String> wordSet, int begin, int countBlankSpace) {
         //以遍历完字符串s作为base case
-        if (indexRear == s.length() - 1) {
+        if (begin == s.length()) {
+            sb.deleteCharAt(sb.length() - 1);//删除末尾添加的空字符
             res.add(sb.toString());
+            return;
         }
-        StringBuilder word = new StringBuilder(s.substring(indexPre, indexRear + 1));
         //在单词字典中查找是否有当前word
-        for (int i = 0; i < wordDict.size(); i++) {
-            if (word.toString() == wordDict.get(i)) {
-                sb.append(word + " ");
-                //修改indexPre和indexRear的值
-                indexPre = indexRear + 1;
-                indexRear =
+        for (int i = begin; i < s.length(); i++) {
+            String temp = s.substring(begin, i + 1);
+            if (wordSet.contains(temp)) {
+                sb.append(temp + ' ');
+                backTrack(res, sb, s, wordSet, i + 1, countBlankSpace + 1);
+                //i在countBlankSpace后又加2是为了将要删除的此单词后面加入的空字符也删除还有一个原因是delete()前包后不包含
+                sb.delete(begin + countBlankSpace, i + 2 + countBlankSpace);
+            } else {
                 continue;
             }
         }
+    }
+    /**
+     * 完全看不懂这个方法是什么意思，，，，，，，
+     * */
+    public List<String> wordBreak2(String s, List<String> wordDict)
+    {
+        Map<String, List<String>> map = new HashMap<>(); //该哈希表用来存储字符串对应的句子列表
+        Set<String> dict = new HashSet<>();
+        for(String str : wordDict) //将词典转换为哈希表，也可以直接用词典的contains方法
+        {
+            dict.add(str);
+        }
+        return helper(s, map, dict);
+    }
 
-        backTrack(res, sb, s, wordDict, lens,indexLens + 1, indexPre, indexRear);
+    public List<String> helper(String s, Map<String, List<String>> map, Set<String> dict)
+    {
+        if(map.containsKey(s)) //首先，看一下字符串s是否已经存在在Map中，如果存在，直接返回对应的值也就是句子列表
+        {
+            return map.get(s);
+        }
+        List<String> cur = new ArrayList<>();
+        if(dict.contains(s)) //这一步很重要，对应着递归的最底层
+        {
+            cur.add(s);
+        }
+        for(int i = 0; i < s.length(); i++)
+        {
+            String subRight = s.substring(i); //找子串
+            if(!dict.contains(subRight))
+            {
+                continue;
+            }
+            // 如果右边的字符串在字典里面
+            List<String> subLeft = helper(s.substring(0, i), map, dict); //递归找左边的子串对应的句子列表
+            List<String> tmpp = append(subLeft, subRight); //将左边的子串返回的句子列表跟右边的单词一一拼接
+            for(String tt : tmpp)
+            {
+                cur.add(tt); //将所有句子加入到结果中
+            }
+        }
+        map.put(s, cur); //放入表里
+        return map.get(s); //返回该字符串对应的句子列表
+    }
+
+    public List<String> append(List<String> tmp, String str)
+    {
+        List<String> build = new ArrayList<>();
+        for(String ss : tmp) //将句子列表中的每一个句子跟单词拼接，然后生成一个新的句子列表
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(ss).append(" ").append(str);
+            build.add(sb.toString());
+        }
+        return build; //返回结果
     }
 }
