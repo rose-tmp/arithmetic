c84e7dd937f6aa3cd4480563b78747daecd701e5
ZwZXXX <wenzhe.zhang@njust.edu.cn>
2020-11-20 16:07:50 +0800

    147. 对链表进行插入排序

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -5,8 +5,7 @@ import zuoPrimary.Swap;
 /**
  * @author - ZwZ
  * @date - 2020/3/22 - 15:22
- * @Description:插入排序
- * 假设一个数组长为N，前i个值是有序的
+ * @Description:插入排序 假设一个数组长为N，前i个值是有序的
  * 把i+1的位置上的数按照大小顺序插入到已经有序的i个值中
  * 时间复杂度：
  * 当数组本身是从小到大有序：O(n)  因为每次内次for条件都不满足，所以不执行，只执行了外层for
@@ -16,13 +15,14 @@ import zuoPrimary.Swap;
  * 工程中的排序，当数据量小的时候，会选择使用插入排序而不是快排和堆排
  */
 public class InsertSort {
-    public void insertSort(int[] arr){
-        if(arr == null || arr.length < 2)
+    public void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2)
             return;
         //前i个值都是有序的
         for (int i = 1; i < arr.length; i++) {
-            //将i+1位置上的数插入到前面有序数组中形成一个新的有序子数组 当arr[j] == arr[j+1]时不交换，所以可以做到稳定
-            for (int j = i - 1; j >0 && arr[j] > arr[j+1] ; j--) {
+            //将i+1位置上的数插入到前面有序数组中形成一个新的有序子数组
+            // 当arr[j] == arr[j+1]时不交换，所以可以做到稳定
+            for (int j = i - 1; j > 0 && arr[j] > arr[j + 1]; j--) {
                 Swap.swap(arr, j, j + 1);
             }
         }
---
+++ src/sort/InsertionSortList.java
@@ -0,0 +1,38 @@
+package sort;
+
+import leetcode.pool.ListNode;
+
+/**
+ * @author - ZwZ
+ * @date - 2020/11/20 - 15:35
+ * @Description:147. 对链表进行插入排序
+ */
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummyNode = new ListNode(Integer.MIN_VALUE);//哑节点 便于将元素插入到头部
+        dummyNode.next = head;
+        ListNode lastNode = head;//有序节点中的最后一个节点
+        ListNode node = head.next;
+        while (node != null) {
+            //直接插入到有序序列的最后一个位置
+            if (lastNode.value <= node.value) {
+                lastNode = lastNode.next;
+            }
+            //从有序序列的头部开始遍历 直到找到node的位置
+            else {
+                ListNode temp = dummyNode;
+                while (temp.next.value <= node.value) {
+                    temp = temp.next;
+                }
+                lastNode.next = node.next;
+                node.next = temp.next;
+                temp.next = node;
+            }
+            node = lastNode.next;
+        }
+        return dummyNode.next;
+    }
+}
