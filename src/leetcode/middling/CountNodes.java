2bfda633967ace5f77bd171968e8e8b148093f35
ZwZXXX <wenzhe.zhang@njust.edu.cn>
2020-11-24 11:59:56 +0800

    222. 完全二叉树的节点个数

---
+++ src/leetcode/middling/CountNodes.java
@@ -0,0 +1,67 @@
+package leetcode.middling;
+
+import leetcode.pool.TreeNode;
+
+/**
+ * @author - ZwZ
+ * @date - 2020/11/24 - 11:36
+ * @Description:222.完全二叉树的节点个数
+ * 要求时间复杂度低于O(N)【前中后遍历的时间复杂度都是O(N)】
+ *
+ * 高度为h的满二叉树的节点个数：2^h - 1
+ */
+public class CountNodes {
+    /**
+     * 思路：
+     * 求出该树的深度h，(一直向左走，走到底)
+     * 从根节点的右子树出发向左走，走到底的高度为h2
+     * 如果h2 = h,那么根节点的左子树一定是满的，直接使用满二叉树节点个数的公式求解，然后再求右子树的高度(其就是一个子问题，所以可以用递归解决)
+     * 如果h2 < h,那么根节点的右子树一定是满的，直接使用满二叉树节点个数公式求解，然后递归求左子树的节点个数
+     * 时间复杂度：这种办法，每一层都只遍历一个节点，一共有logN层
+     * 而每次遍历某个节点的时候都会沿着左节点走到底，所以整体的时间复杂度为：O((logN)^2)
+     * (和zuoPrimary.shu.CompleteBinaryTreeNodeSum思路一模一样，只是它实现方式复杂 所以算法最重要的是学思想，然后自己去实现，每个人编程的功底以及对算法运行过程理解的不同，代码的逻辑都不尽相同，要在自己的编码风格下去实现一个算法思想  才算是真的get)
+     * */
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return count(root);
+    }
+    public int count(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int high = getHigh(root);
+        int highR = getHigh(root.right) + 1;
+        //此时根节点的左子树为满二叉树
+        if(highR == high){
+            return (1 << (high - 1)) + count(root.right);
+        }
+        //右子树为满二叉树
+        else{
+            return (1 << (highR - 1)) + count(root.left);
+        }
+    }
+    /**
+     * 得到树高度
+     */
+    public int getHigh(TreeNode root) {
+        int high = 0;
+        while(root != null){
+            high++;
+            root = root.left;
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        CountNodes count = new CountNodes();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        count.countNodes(root);
+    }
+}
--- src/zuoPrimary/shu/CompleteBinaryTreeNodeSum.java
+++ src/zuoPrimary/shu/CompleteBinaryTreeNodeSum.java
@@ -1,7 +1,5 @@
 package zuoPrimary.shu;
 
-import zuoPrimary.Swap;
-
 /**
  * @author - ZwZ
  * @date - 2020/3/25 - 22:46
@@ -9,21 +7,19 @@ import zuoPrimary.Swap;
  * 高度为h的满二叉树的节点个数：2^h - 1
  * 思路：
  * 求出该树的深度h，(一直向左走，走到底)
- * 从该节点的右子树出发向左走，走到底的高度为h2
- * 如果h2 = h,那么该节点的左子树一定是满的，直接使用满二叉树节点个数的公式求解，然后再求右子树的高度(其就是一个子问题，所以可以用递归解决)
- * 如果h2 < h,那么该节点的右子树一定是满的，直接使用满二叉树节点个数公式求解，然后递归求左子树的节点个数
+ * 从根节点的右子树出发向左走，走到底的高度为h2
+ * 如果h2 = h,那么根节点的左子树一定是满的，直接使用满二叉树节点个数的公式求解，然后再求右子树的高度(其就是一个子问题，所以可以用递归解决)
+ * 如果h2 < h,那么根节点的右子树一定是满的，直接使用满二叉树节点个数公式求解，然后递归求左子树的节点个数
  *
  * 时间复杂度：这种办法，每一层都只遍历一个节点，一共有logN层
  * 而每次遍历某个节点的时候都会沿着左节点走到底，所以整体的时间复杂度为：O((logN)^2)
  */
 public class CompleteBinaryTreeNodeSum {
     /**
-     *  
-     *
      * @Author: ZwZ
-     * @Description:  
-     * @Param: [node] 
-     * @return: int 
+     * @Description:
+     * @Param: [node]
+     * @return: int
      * @Date: 2020/3/26-11:30
      */
     public int nodeSum(TreeNode node) {
@@ -33,13 +29,11 @@ public class CompleteBinaryTreeNodeSum {
     }
 
     /**
-     *  
-     *
      * @Author: ZwZ
      * @Description:level：当前节点所在的层数，high : 整棵树的高度(始终不变)
-     * @Param: [node, level, high] 
-     * @return: int 以node为根节点的子树一共有多少节点
-     * @Date: 2020/3/26-11:42
+     * @Param: [node, level, high]
+     * @return: int以node为根节点的子树一共有多少节点
+     * @Date: 2020/3/26-1142
      */
     private int bs(TreeNode node, int level, int high) {
         if (level == high)
@@ -52,12 +46,10 @@ public class CompleteBinaryTreeNodeSum {
     }
 
     /**
-     *  
-     *
      * @Author: ZwZ
-     * @Description:求以node为根节点的树的整体高度(相对于整棵树的根节点) level：node当前高度 
-     * @Param: [node, level] 
-     * @return: int 
+     * @Description:求以node为根节点的树的整体高度(相对于整棵树的根节点) level：node当前高度
+     * @Param: [node, level]
+     * @return: int
      * @Date: 2020/3/26-11:51
      */
     private int getHigh(TreeNode node, int level) {
@@ -81,32 +73,4 @@ public class CompleteBinaryTreeNodeSum {
         System.out.println(sum.nodeSum(root));
     }
 
-    /**
-     * @author - ZwZ
-     * @date - 2020/3/22 - 15:03
-     * @Description:选择排序
-     * (由于时间复杂度高，这种排序算法几乎没有人使用在工程中)
-     * 0-----n-1选出最小值放在0位置
-     * 1-----n-1选出最小值放在1位置
-     * 2-----n-1选出最小值放在2位置
-     * ....
-     * 时间复杂度和数据本身状况无关，恒为：O(n^2)
-     *
-     * 无论如何都做不到稳定 举例：5 5 5 5 4 0 2
-     */
-    public static class SelectSort {
-        public void selectSort(int[] arr){
-            if(arr == null || arr.length < 2)
-                return;
-            for (int start = 0; start < arr.length - 1; start++) {
-                int min = start;
-                //找最小值的下标
-                for (int i = start + 1; i < arr.length; i++) {
-                    min = arr[i] < arr[min] ? i : min;
-                }
-                //交换
-                Swap.swap(arr, start, min);
-            }
-        }
-    }
 }
